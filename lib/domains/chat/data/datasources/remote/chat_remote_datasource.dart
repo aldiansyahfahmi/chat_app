fa0a46201ad6eed013ed3fb929107de3bf969bbc
aldiansyahfahmi <aldiansyahfahmi6@gmail.com>
2023-08-10 17:06:05 +0800

    wip send message

--- lib/domains/chat/data/datasources/remote/chat_remote_datasource.dart
+++ lib/domains/chat/data/datasources/remote/chat_remote_datasource.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:chat_app/domains/chat/data/models/body/create_chat_room_request_dto.dart';
 import 'package:chat_app/services/firestore_service.dart';
+import 'package:chat_app/shared_libraries/utils/constants/app_constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class ChatRemoteDataSource {
@@ -16,53 +17,54 @@ class ChatRemoteDataSourceImpl implements ChatRemoteDataSource {
   Future<void> sendMessage(
       {required CreateChatRoomRequestDto requestDto}) async {
     try {
-      final chatCol = await firestoreService.chatCollection.get();
-      if (chatCol.docs.isEmpty) {
-        log('Chat kosong');
-        final result =
+      final chatDoc = await firestoreService.chatCollection.where(
+        'participants',
+        whereIn: [
+          [
+            auth.currentUser!.email,
+            requestDto.toEmail,
+          ],
+          [
+            requestDto.toEmail,
+            auth.currentUser!.email,
+          ],
+        ],
+      ).get();
+
+      if (chatDoc.docs.isEmpty) {
+        final createChat =
             await firestoreService.chatCollection.add(requestDto.toJson());
         await firestoreService.usersCollection
-            .doc(
-          auth.currentUser!.email,
-        )
-            .update({
-          'chats': [
-            {
-              'chat_id': result.id,
-            },
-          ],
+            .doc(auth.currentUser!.email)
+            .collection(AppConstants.appCollection.chat)
+            .add({
+          'chat_id': createChat.id,
+          'chat_with': requestDto.toEmail,
         });
-
         await firestoreService.usersCollection
-            .doc(
-          requestDto.toEmail,
-        )
-            .update({
-          'chats': [
-            {
-              'chat_id': result.id,
-            },
-          ],
+            .doc(requestDto.toEmail)
+            .collection(AppConstants.appCollection.chat)
+            .add({
+          'chat_id': createChat.id,
+          'chat_with': auth.currentUser!.email,
         });
       } else {
-        log('Chat tidak kosong');
-        final chatDoc = await firestoreService.chatCollection.where(
-          'participants',
-          whereIn: [
-            [
-              auth.currentUser!.email,
-              requestDto.toEmail,
-            ],
-            [
-              requestDto.toEmail,
-              auth.currentUser!.email,
-            ],
-          ],
-        ).get();
-
-        if (chatDoc.docs.isEmpty) {
-          await firestoreService.chatCollection.add(requestDto.toJson());
-        }
+        String chatId = '';
+        final myChat = await firestoreService.usersCollection
+            .doc(auth.currentUser!.email)
+            .collection(AppConstants.appCollection.chat)
+            .where('chat_with', isEqualTo: requestDto.toEmail)
+            .get();
+        log(myChat.docs.first.data().values.toString());
+        chatId = myChat.docs.first.data()['chat_id'];
+        log('chat ID : $chatId');
+        await firestoreService.chatCollection
+            .doc(chatId)
+            .collection('messages')
+            .add({
+          'message': 'Haloooooo',
+          'sender': auth.currentUser!.email,
+        });
       }
     } catch (e) {
       rethrow;
